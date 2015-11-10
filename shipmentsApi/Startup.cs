b9e887a496644dc4218291195f43e974e8882546
sebastian correa morales <correa.m.sebastian@gmail.com>
2015-11-10 16:59:28 -0500

    getting a token an later using to authoriza our get mehotd  on shipments controller

--- shipmentsApi/Startup.cs
+++ shipmentsApi/Startup.cs
@@ -4,20 +4,54 @@ using Owin;
 using shipmentsApi;
 using shipmentsApi.App_Start;
 using System.Web.Http;
+using System;
+using System.Configuration;
+using Microsoft.Owin.Security.DataHandler.Encoder;
+using Microsoft.Owin.Security.Jwt;
+using Microsoft.Owin.Security;
 
 [assembly: OwinStartup(typeof(Startup))]
 
 namespace shipmentsApi
 {
-    public class Startup
+    public partial class Startup
     {
 
         public void Configuration(IAppBuilder app)
         {
             var config = new HttpConfiguration();
             WebApiConfig.Register(config);
+
+            ConfigureAuthZero(app);
+
             app.UseCors(CorsOptions.AllowAll);
             app.UseWebApi(config);
+        }
+
+
+    }
+
+    public partial class Startup
+    {
+        private void ConfigureAuthZero(IAppBuilder app)
+        {
+            var issuer = "https://" + ConfigurationManager.AppSettings["auth0:Domain"] + "/";
+            var audience = ConfigurationManager.AppSettings["auth0:ClientId"];
+            var secret = TextEncodings.Base64.Encode(
+                TextEncodings.Base64Url.Decode(ConfigurationManager.AppSettings["auth0:ClientSecret"]));
+
+            var jwtOptions = new JwtBearerAuthenticationOptions
+            {
+                AuthenticationMode = AuthenticationMode.Active,
+                AllowedAudiences = new[] { audience },
+                IssuerSecurityTokenProviders = new[]
+                {
+                    new SymmetricKeyIssuerSecurityTokenProvider(issuer, secret)
+                }
+            };
+
+            app.UseJwtBearerAuthentication(jwtOptions);
+
         }
     }
 }
