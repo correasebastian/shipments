c7cd74ef81e8d2e1ad6269bd7172810f661a2547
sebastian correa morales <correa.m.sebastian@gmail.com>
2015-11-10 15:34:50 -0500

    defining routes and the first controller

---
+++ shipmentsApi/App_Start/WebApiConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace shipmentsApi.App_Start
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+            config.Routes.MapHttpRoute(
+                "DefaultApi", "api/{controller}/{id}",
+                new { id = RouteParameter.Optional });
+
+        }
+    }
+}
---
+++ shipmentsApi/Startup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Owin;
+using shipmentsApi;
+using shipmentsApi.App_Start;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace shipmentsApi
+{
+    public class Startup
+    {
+
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseCors(CorsOptions.AllowAll);
+            app.UseWebApi(config);
+        }
+    }
+}
